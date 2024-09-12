8f536cd363e5c36f09bdd148de16d94406f8a750
rajeevvc <rajeev.outlook.com>
2024-09-12 10:46:37 +0530

    version 2

--- lambda_function.py
+++ lambda_function.py
@@ -5,4 +5,5 @@ def lambda_handler(event,context):
     ip = get('https://api.ipify.org').text
     print('My public ip is: {}'.format(ip))
     print('Version 1')
+    print('Version 2')
     return 'My Public IP is:' + ip
